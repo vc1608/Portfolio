8ad69d115f1f3135d0749864c7a6bac208798957
vc1608 <92731460+vc1608@users.noreply.github.com>
2021-10-22 20:17:54 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,33 @@
+const cacheName = 'my-cache';
+
+// put the static assets and routes you want to cache here
+const filesToCache = [
+  '/',
+  '/about',
+  '/index.html',
+  '/about.html',
+  '/css/styles.css',
+  '/js/app.js',
+  '/img/logo.png'
+];
+
+// the event handler for the activate event
+self.addEventListener('activate', e => self.clients.claim());
+
+// the event handler for the install event 
+// typically used to cache assets
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName)
+    .then(cache => cache.addAll(filesToCache))
+  );
+});
+
+// the fetch event handler, to intercept requests and serve all 
+// static assets from the cache
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request)
+    .then(response => response ? response : fetch(e.request))
+  )
+});
